378a7cae733c774f88d8c1b67d29225cc7579c36
Henry <seventyone7100@gmail.com>
2025-05-20 13:02:03 +0300

    Animations Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem";
+import { motion, Reorder } from "framer-motion";
 
 type Todo = {
   id: number;
@@ -53,7 +54,6 @@ function App() {
   }
 
   function editTodoFunc(id: number, e: React.ChangeEvent<HTMLInputElement>) {
-    // e.target.focus();
     const editedText = e.target.value;
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
@@ -67,7 +67,7 @@ function App() {
   return (
     <div className="w-full h-screen bg-[url(/images/13.jpg)] bg-cover bg-center _bg-gray-900 flex items-center justify-center">
       <div className="px-4 py-4">
-        <div className="flex flex-col items-center gap-x-12 px-4 py-4 rounded-3xl shadow-indigo-950/45 shadow-2xl border-[1px] border-rose-300 backdrop-blur-[10px]">
+        <div className="flex flex-col items-center gap-x-12 px-8 py-8 rounded-3xl shadow-indigo-950/85 shadow-2xl border-[1px] border-rose-300 backdrop-blur-[10px] bg-slate-700/20">
           <h1 className="text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold tracking-tight text-white text-center">
             Welcome to Todo App!
           </h1>
@@ -78,33 +78,49 @@ function App() {
               onChange={setTodoTextFunc}
               onKeyDown={handleKeyDown}
               value={todoText}
+              placeholder="Enter What You Want To Do..."
               className="w-full h-full outline-0 bg-slate-600/50 rounded-xl text-white caret-amber-500 px-3 py-2 text-xl shadow-xl shadow-indigo-950/40"
             />
             <button
               onClick={addTodoFunc}
-              className="h-full font-bold text-sm lg:text-xl cursor-pointer px-7 py-2 bg-amber-500 rounded-xl text-white"
+              className="h-full font-bold text-sm lg:text-xl cursor-pointer px-7 py-2 bg-amber-400/40 rounded-xl text-white"
             >
               Add
             </button>
           </div>
         </div>
-        {/* container */}
         <div className="mt-12">
-          <ul className="flex flex-col gap-y-4">
-            {todos.map((todo, idx) => (
-              <TodoItem
-                key={idx}
-                id={todo.id}
-                completed={todo.completed}
-                text={todo.text}
-                deleteTodoFunc={deleteTodoFunc}
-                toggleCompletedFunc={toggleCompletedFunc}
-                editTodoFunc={editTodoFunc}
-                setEditingId={setEditingId}
-                editingId={editingId}
-              />
+          <Reorder.Group
+            axis="y"
+            values={todos}
+            onReorder={setTodos}
+            className="flex flex-col gap-y-4"
+          >
+            {todos.map((todo) => (
+              <Reorder.Item
+                key={todo.id}
+                value={todo}
+                className="cursor-grab active:cursor-grabbing"
+                layout
+                transition={{ duration: 0.25 }}
+                initial={{ scale: 1 }}
+                animate={{ scale: 1 }}
+                whileDrag={{ scale: 1.03 }}
+                exit={{ scale: 1 }}
+              >
+                <TodoItem
+                  id={todo.id}
+                  completed={todo.completed}
+                  text={todo.text}
+                  deleteTodoFunc={deleteTodoFunc}
+                  toggleCompletedFunc={toggleCompletedFunc}
+                  editTodoFunc={editTodoFunc}
+                  setEditingId={setEditingId}
+                  editingId={editingId}
+                />
+              </Reorder.Item>
             ))}
-          </ul>
+          </Reorder.Group>
         </div>
       </div>
     </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from "react";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
 
 type Props = {
   id: number;
@@ -27,25 +29,26 @@ export default function TodoItem({
     if (editingId === id && inputRef.current) {
       inputRef.current.focus();
     }
-  }, [editingId]);
+  }, [editingId, id]);
 
   return (
     <div className="relative select-none">
-      <li className="relative flex items-center justify-between h-[3rem] rounded-xl overflow-hidden bg-slate-300/40 backdrop-blur-[5px] shadow-xl shadow-slate-700/30">
+      <li className="relative flex items-center justify-between h-[3rem] rounded-xl overflow-hidden bg-slate-700/40 _bg-slate-300/40 backdrop-blur-[8px] shadow-xl shadow-slate-900/50">
         {editingId !== id ? (
           <div
-            onClick={() => toggleCompletedFunc(id)}
+            // onClick={() => toggleCompletedFunc(id)}
             className="w-full h-full flex gap-x-2 items-center pl-2 pr-3"
           >
             <input
               type="checkbox"
-              // onChange={() => toggleCompletedFunc(id)}
-              className="border-none outline-none w-[20px] _h-full aspect-square _rounded-full"
+              checked={completed}
+              onChange={() => toggleCompletedFunc(id)}
+              className="cursor-pointer border-none outline-none w-[20px] _h-full aspect-square _rounded-full"
             />
             <p
               className={`${
                 completed
-                  ? "line-through decoration-slate-800 decoration-[4px] text-8xl"
+                  ? "line-through decoration-slate-700 decoration-[4px] text-8xl text-slate-400"
                   : "no-underline"
               } font-semibold text-sm lg:text-xl xl:text-2xl text-slate-100 line-clamp-1`}
             >
@@ -65,20 +68,20 @@ export default function TodoItem({
             }}
             onBlur={() => setEditingId(null)}
             onChange={(e) => editTodoFunc(id, e)}
-            className="w-full h-full relative _absolute _top-full _left-0 z-20 px-2 text-lg tracking-wide rounded-xl bg-white outline-none border-2 border-amber-500 shadow-2xl shadow-indigo-600/40"
+            className="w-full h-full relative _absolute _top-full _left-0 z-20 px-2 text-xl tracking-wide rounded-xl bg-amber-500/20 outline-none border-2 border-amber-500 shadow-2xl shadow-indigo-600/40 text-white"
           />
         )}
         <button
           onClick={() => setEditingId(id)}
-          className="relative z-30 h-full font-bold cursor-pointer px-3 lg:px-5 xl:px-7 py-2 bg-amber-500 text-white text-sm lg:text-lg xl:text-xl"
+          className="relative z-30 h-[70%] _h-full rounded-2xl font-bold cursor-pointer px-3 lg:px-3 xl:px-5 py-2 bg-amber-900/20 _bg-amber-500 text-white text-sm lg:text-lg xl:text-xl flex items-center justify-center border-[1px] border-amber-400"
         >
-          Edit
+          <FaEdit className="text-amber-400" />
         </button>
         <button
           onClick={() => deleteTodoFunc(id)}
-          className="relative z-30 h-full font-bold cursor-pointer px-3 lg:px-5 xl:px-7 py-2 bg-rose-500 text-white text-sm lg:text-lg xl:text-xl"
+          className="relative z-30 h-[70%] _h-full rounded-2xl font-bold cursor-pointer px-3 lg:px-3 xl:px-5 py-2 bg-rose-900/15 text-white text-sm lg:text-lg xl:text-xl mx-3 border-[1px] border-rose-400"
         >
-          Delete
+          <FaTrashAlt className="text-rose-300" />
         </button>
       </li>
     </div>
